00f444be3b2a356ba49dcec22b6323c2d426c937
Bobby de Vos <bobby_devos@sil.org>
2024-06-05 06:57:40 -0600

    Process directories of test data

--- src/thefoxUtils/linespacing.py
+++ src/thefoxUtils/linespacing.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import argparse
+import glob
+import os.path
 
 import uharfbuzz as hb
 from fontTools.pens.boundsPen import BoundsPen
@@ -11,29 +13,50 @@ from thefoxUtils import unidump
 
 def main():
     parser = argparse.ArgumentParser(description='Calculations for linespacing')
-    parser.add_argument('text', help='text file of test data')
+    parser.add_argument('test', help='directory of test data')
     parser.add_argument('fonts', help='fonts to read', nargs='+')
+    parser.add_argument('-e', '--extra', help='extra directory of test data')
     args = parser.parse_args()
 
-    words = find_words(args.text)
-    lowest_extremas, highest_extremas = find_extrema(words, args.fonts)
+    test_words = find_words(args.test)
+    test_words += find_words(args.extra)
+    lowest_extremas, highest_extremas = find_extrema(test_words, args.fonts)
     report(lowest_extremas)
     report(highest_extremas)
 
 
-def find_words(text_file):
+class TestWord():
+    """Test word"""
+
+    def __init__(self, text, line_num, word_num, text_filename):
+        self.text = text
+        self.line_num = line_num
+        self.word_num = word_num
+        self.text_filename = text_filename
+
+
+def find_words(test_dir):
     """Make a list of words from the text"""
 
-    word_list = []
-    with open(text_file) as text:
-        for line in text:
-            words = line.split()
-            for word in words:
-                word_list.append(word)
-    return word_list
+    if not test_dir:
+        return []
+    test_words = []
+    text_files = glob.glob('**/*.*txt', root_dir=test_dir, recursive=True)
+    for text_file in text_files:
+        with open(os.path.join(test_dir, text_file)) as text:
+            line_num = 0
+            for line in text:
+                words = line.split()
+                word_num = 0
+                for word in words:
+                    test_word = TestWord(word, line_num, word_num, text_file)
+                    test_words.append(test_word)
+                    word_num += 1
+                line_num += 1
+    return test_words
 
 
-def find_extrema(words, font_filenames):
+def find_extrema(test_words, font_filenames):
     """Find extrema in TTF files"""
 
     lowest_extremas = []
@@ -50,9 +73,9 @@ def find_extrema(words, font_filenames):
         hb_face = hb.Face(hb_blob)
         hb_font = hb.Font(hb_face)
 
-        for word in words:
+        for test_word in test_words:
             buf = hb.Buffer()
-            buf.add_str(word)
+            buf.add_str(test_word.text)
             buf.guess_segment_properties()
             features = {}
             hb.shape(hb_font, buf, features)
@@ -71,13 +94,13 @@ def find_extrema(words, font_filenames):
                     lowest = low
                     lowest_extremas = []
                 if low == lowest:
-                    extrema = Extrema(word, low, font_filename)
+                    extrema = Extrema(test_word, low, font_filename)
                     lowest_extremas.append(extrema)
                 if high > highest:
                     highest = high
                     highest_extremas = []
                 if high == highest:
-                    extrema = Extrema(word, high, font_filename)
+                    extrema = Extrema(test_word, high, font_filename)
                     highest_extremas.append(extrema)
     return lowest_extremas, highest_extremas
 
@@ -88,19 +111,19 @@ def report(extremas):
         print(extrema.report() + '\n')
 
 
-class Extrema():
+class Extrema:
     """Data about an extrema"""
 
-    def __init__(self, word, level, font_filename):
-        self.word = word
+    def __init__(self, test_word, level, font_filename):
+        self.test_word = test_word
         self.level = level
         self.font_filename = font_filename
 
     def report(self):
         escape = ''
-        for char in self.word:
+        for char in self.test_word.text:
             escape += unidump.escape(char)
-        return f'{self.word}\n{escape}\nat {self.level} in {self.font_filename}'
+        return f'{self.test_word.text}\n{escape}\nat {self.level} from {self.test_word.text_filename}:{self.test_word.line_num}:{self.test_word.word_num} in {self.font_filename}'
 
 
 if __name__ == '__main__':
